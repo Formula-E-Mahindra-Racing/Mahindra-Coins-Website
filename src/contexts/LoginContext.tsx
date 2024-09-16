789cae23c708bacf879fb6153b9440aa187c998f
gh-johnny <jmfurtadoromero@gmail.com>
2024-09-15 21:10:22 -0300

    build config

--- src/contexts/LoginContext.tsx
+++ src/contexts/LoginContext.tsx
@@ -1,5 +1,4 @@
 import { createContext, Dispatch, ReactNode, SetStateAction, useState } from 'react'
-import { useLocation } from 'react-router-dom'
 
 interface AuthContext {
     isAuthenticated: boolean
@@ -13,7 +12,6 @@ export const AuthContext = createContext({} as AuthContext)
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
     const [isAuthenticated, setIsAuthenticated] = useState(true)
-    const location = useLocation()
 
     const login = () => setIsAuthenticated(true)
     const logout = () => setIsAuthenticated(false)
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,8 +3,6 @@ import { Link } from "react-router-dom"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
-import { useContext } from "react"
-import { LoginContext } from "@/contexts/LoginContext"
 
 export const description =
   "A login page with two columns. The first column has the login form with email and password. There's a Forgot your passwork link and a link to sign up if you do not have an account. The second column has a cover image."
