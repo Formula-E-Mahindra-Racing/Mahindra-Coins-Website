d6db1e71c77b271130b2512eceadae749367a331
gh-johnny <jmfurtadoromero@gmail.com>
2024-09-22 01:40:02 -0300

    style: mahindra banner (header) on top

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,10 +3,10 @@ import mahindraLogoMobile from "../assets/mahindra-logo-mobile.png"
 
 export default function Header() {
     return (
-        <header className="fixed flex w-dvw h-[10vh]">
+        <header className="fixed z-50 flex w-dvw h-[10vh]">
             <section className="relative w-[15vw] max-h-[120px]">
                 <div className="flex absolute h-[100px] sm:h-full sm:-right-[2vw] min-w-[65px] w-[17.5vw] sm:w-[15vw] bg-[#b81d1a] sm:-skew-x-[30deg] p-4 justify-center items-center">
-                <div className="hidden sm:flex absolute -left-[2vw] bg-[#b81d1a] -z-10 w-[calc(100%-2vw)] h-full border-blue-500 sm:skew-x-[30deg]"/>
+                <div className="hidden sm:flex absolute -left-[2vw] bg-[#b81d1a] w-[calc(100%-2vw)] h-full border-blue-500 sm:skew-x-[30deg]"/>
                     <img
                         src={mahindraLogoWhite}
                         className="hidden sm:block min-w-[12vw] skew-x-[30deg] max-h-[60px] absolute top-1/2 left-1/2 -translate-x-[calc(50%+1.5vw)] -translate-y-1/2 border-blue-500"
